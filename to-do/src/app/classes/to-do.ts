2090a88fe2383f5a06e16d2e7af52eb4c87e2573
Olga Andrievich <olgakuz2302@gmail.com>
2023-06-14 23:20:33 +0300

    added to-do class

---
+++ to-do/src/app/classes/to-do.ts
@@ -0,0 +1,9 @@
+export class ToDo {
+  id?: number;
+  todo: string = '';
+  status: 'ordinary' | 'important' | 'done' = 'ordinary';
+  
+  constructor(values: Object = {}) {
+    Object.assign(this, values);
+  }
+}
